cc50fb3ffa808be6a7ae50ea840c400fe15207a3
tianwen <1097371329@qq.com>
2017-11-23 20:56:13 +0800

    Preparing for charts.html layout

--- mysite/draw/templates/draw/static/js/drawCharts.js
+++ mysite/draw/templates/draw/static/js/drawCharts.js
@@ -1,95 +1,94 @@
 $(document).ready(function(){
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-    var formatPercent = d3.format(".0%");
+var formatPercent = d3.format(".0%");
 
-    var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .1, 1);
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1, 1);
 
-    var y = d3.scale.linear()
-        .range([height, 0]);
+var y = d3.scale.linear()
+    .range([height, 0]);
 
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .tickFormat(formatPercent);
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(formatPercent);
 
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.tsv("../data.tsv", function(error, data) {
+d3.tsv("../data.tsv", function(error, data) {
 
-      data.forEach(function(d) {
-        console.log(d);
-        d.frequency = +d.frequency;
-      });
+  data.forEach(function(d) {
+    d.frequency = +d.frequency;
+  });
 
-      x.domain(data.map(function(d) { return d.letter; }));
-      y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+  x.domain(data.map(function(d) { return d.letter; }));
+  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("Frequency");
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
 
-      svg.selectAll(".bar")
-          .data(data)
-        .enter().append("rect")
-          .attr("class", "bar")
-          .attr("x", function(d) { return x(d.letter); })
-          .attr("width", x.rangeBand())
-          .attr("y", function(d) { return y(d.frequency); })
-          .attr("height", function(d) { return height - y(d.frequency); });
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.letter); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) {return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); });
 
-      d3.select("input").on("change", change);
+  d3.select("input").on("change", change);
 
-      var sortTimeout = setTimeout(function() {
-        d3.select("input").property("checked", true).each(change);
-      }, 2000);
+  var sortTimeout = setTimeout(function() {
+    d3.select("input").property("checked", true).each(change);
+  }, 2000);
 
-      function change() {
-        clearTimeout(sortTimeout);
+  function change() {
+    clearTimeout(sortTimeout);
 
-        // Copy-on-write since tweens are evaluated after a delay.
-        var x0 = x.domain(data.sort(this.checked
-            ? function(a, b) { return b.frequency - a.frequency; }
-            : function(a, b) { return d3.ascending(a.letter, b.letter); })
-            .map(function(d) { return d.letter; }))
-            .copy();
+    // Copy-on-write since tweens are evaluated after a delay.
+    var x0 = x.domain(data.sort(this.checked
+        ? function(a, b) { return b.frequency - a.frequency; }
+        : function(a, b) { return d3.ascending(a.letter, b.letter); })
+        .map(function(d) { return d.letter; }))
+        .copy();
 
-        svg.selectAll(".bar")
-            .sort(function(a, b) { return x0(a.letter) - x0(b.letter); });
+    svg.selectAll(".bar")
+        .sort(function(a, b) { return x0(a.letter) - x0(b.letter); });
 
-        var transition = svg.transition().duration(750),
-            delay = function(d, i) { return i * 50; };
+    var transition = svg.transition().duration(750),
+        delay = function(d, i) { return i * 50; };
 
-        transition.selectAll(".bar")
-            .delay(delay)
-            .attr("x", function(d) { return x0(d.letter); });
+    transition.selectAll(".bar")
+        .delay(delay)
+        .attr("x", function(d) { return x0(d.letter); });
 
-        transition.select(".x.axis")
-            .call(xAxis)
-          .selectAll("g")
-            .delay(delay);
-      }
-    });
-})
+    transition.select(".x.axis")
+        .call(xAxis)
+      .selectAll("g")
+        .delay(delay);
+  }
+});
+});
